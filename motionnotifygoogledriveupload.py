b427f5fb851cd2401bfe56cfc9b2062fd5d946c8
Peter Clarke <pete@clarkez.co.uk>
2015-10-13 22:23:24 +0100

    Updated Folder Search to use folderIds and ensure parent folder is correct. Updated permissions to use owner by default for writers. Fixed up issues with logging

--- motionnotifygoogledriveupload.py
+++ motionnotifygoogledriveupload.py
@@ -7,11 +7,13 @@ from datetime import datetime
 
 from oauth2client.client import SignedJwtAssertionCredentials
 
-logger = logging.getLogger( 'MotionNotifyGoogleUpload')
+logger = logging.getLogger( 'MotionNotify.GoogleUpload')
+#logger.setLevel( logging.DEBUG )
 
 class MotionNotifyGoogleUpload:
     @staticmethod
     def authenticate(config):
+        logger.debug("OAuth2 Authentication - Starting")
         svc_user_id = config.get('drive', 'service_user_email')
         svc_scope = "https://www.googleapis.com/auth/drive"
         svc_key_file = config.get('drive', 'key_file')
@@ -20,27 +22,32 @@ class MotionNotifyGoogleUpload:
         gcredentials.authorize(httplib2.Http())
         gauth = GoogleAuth()
         gauth.credentials = gcredentials
+        logger.debug("OAuth2 Authentication - Complete")
         return gauth
 
     @staticmethod
-    def _get_folder_resource(drive,folder):
+    def _get_folder_resource(drive,folder_name,folder_id):
         """Find and return the resource whose title matches the given folder."""
         try:
-            file_list = drive.ListFile({'q': "title='{}' and mimeType contains 'application/vnd.google-apps.folder' and trashed=false".format(folder)}).GetList()
-            if len(file_list) > 0:
-                return file_list[0]
-            else:
-                return None
+            myfile = drive.CreateFile({'id': folder_id})
+            logger.debug ("Found Parent Folder title: {}, mimeType: {}".format(myfile['title'], myfile['mimeType']) )
+            return  myfile
+
+            #file_list = drive.ListFile({'q': "title='{}' and mimeType contains 'application/vnd.google-apps.folder' and trashed=false".format(folder_name)}).GetList()
+            #if len(file_list) > 0:
+            #    return file_list[0]
+            #else:
+            #    return None
         except IndexError:
             return None
         except:
             return None
 
     @staticmethod
-    def _get_datefolder_resource(drive,formatted_date):
+    def _get_datefolder_resource(drive,formatted_date, parent_id):
         """Find and return the resource whose title matches the given folder."""
         try:
-            file_list = drive.ListFile({'q': "title='{}' and mimeType contains 'application/vnd.google-apps.folder' and trashed=false".format(formatted_date)}).GetList()
+            file_list = drive.ListFile({'q': "title='{}' and '{}' in parents and mimeType contains 'application/vnd.google-apps.folder' and trashed=false".format(formatted_date,parent_id)}).GetList()
             if len(file_list) > 0:
                 return file_list[0]
             else:
@@ -61,18 +68,18 @@ class MotionNotifyGoogleUpload:
         new_folder = drive.CreateFile({'title':'{}'.format(sfldname),
                                    'mimeType':'application/vnd.google-apps.folder'})
         if folder is not None:
-            new_folder['parents'] = [{"id": folder['id']}]
+            new_folder['parents'] = [{"kind": "drive#fileLink","id": folder['id']}]
 
         permissions = []
         owner_permission = MotionNotifyGoogleUpload.create_permision(owner,"owner")
-        write_permissions = [MotionNotifyGoogleUpload.create_permision(x,"writer") for x in writers]
+        write_permissions = [MotionNotifyGoogleUpload.create_permision(x,"owner") for x in writers]
         read_permissions  = [MotionNotifyGoogleUpload.create_permision(x,"reader") for x in readers]
 
         permissions.append(owner_permission)
         permissions.extend(write_permissions)
         permissions.extend(read_permissions)
 
-        print('Creating Folder {} with permissions {}'.format(sfldname, permissions))
+        logger.debug('Creating Folder {} with permissions {}'.format(sfldname, permissions))
 
         new_folder["permissions"] = permissions
         new_folder.Upload()
@@ -92,20 +99,25 @@ class MotionNotifyGoogleUpload:
         owner = config.get('gmail','user')
         writers = filter(lambda x:len(x)>0, [x.strip() for x in config.get('drive','write_users').split(",")] )
         readers = filter(lambda x:len(x)>0, [x.strip() for x in config.get('drive','read_users').split(",")] )
-        writers.append(owner)
+        #Keep Owner Seperate # Remove Just in case
+        if owner in writers:
+            writers.remove(owner);
 
         # Check Root Folder Exists
-        folder_resource = MotionNotifyGoogleUpload._get_folder_resource(drive,folder_name)
+        folder_resource = MotionNotifyGoogleUpload._get_folder_resource(drive,folder_name,folder_id)
         if not folder_resource:
-            logger.info('Creating Folder {}'.format(folder_name))
-            folder_resource = MotionNotifyGoogleUpload.create_subfolder(drive,None,folder_name,owner,readers, writers)
-            # raise Exception('Could not find the %s folder' % self.folder)
+            #logger.info('Creating Folder {}'.format(folder_name))
+            # This seemed to create lots of folders I'd no access to, so changing to an Exception
+            # Might need some way to set it to the root of the gmail user and not the service user
+            #folder_resource = MotionNotifyGoogleUpload.create_subfolder(drive,None,folder_name,owner,readers, writers)
+            logger.error("Could not find the {} folder {}".format(folder_name,folder_id) )
+            raise Exception("Could not find the {} folder {}".format(folder_name,folder_id) )
 
         logger.debug('Using Folder {} {}'.format(folder_name,folder_resource['id']))
 
         # Check Date Folder Exists & Create / Use as needed
-        senddate=datetime.strftime(datetime.now(), config.get('drive', 'dateformat'))
-        datefolder_resource = MotionNotifyGoogleUpload._get_datefolder_resource(drive,senddate)
+        senddate=(datetime.strftime(datetime.now(), config.get('drive', 'dateformat')))
+        datefolder_resource = MotionNotifyGoogleUpload._get_datefolder_resource(drive,senddate,folder_resource['id'])
         if not datefolder_resource:
             logger.info('Creating Date Folder {}'.format(senddate))
             datefolder_resource = MotionNotifyGoogleUpload.create_subfolder(drive,folder_resource,senddate,owner,readers, writers)
