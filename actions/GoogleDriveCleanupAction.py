c586bef39205a9297cc1485ecad1295a7bdd050e
amdean <amdean@gmail.com>
2017-01-09 22:05:54 -0600

    Issue #7 - Implemented Google drive removal

--- actions/GoogleDriveCleanupAction.py
+++ actions/GoogleDriveCleanupAction.py
@@ -1,34 +1,41 @@
-from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import GoogleDriveActionBase
+from datetime import datetime, timedelta
 
 import logging.handlers
 
 logger = logging.getLogger('MotionNotify')
 
 
-class GoogleDriveCleanupAction():
+class GoogleDriveCleanupAction:
     @staticmethod
     def do_event_start_action(config, motion_event):
         logger.info("Motionevent_id:" + motion_event.event_id + " GoogleDriveCleanupAction event start")
-        motion_event.upload_url = GoogleDriveCleanupAction.cleanup(config)
+        GoogleDriveCleanupAction.cleanup(config)
 
     @staticmethod
     def do_event_end_action(config, motion_event):
         logger.info("Motionevent_id:" + motion_event.event_id + " GoogleDriveCleanupAction event end")
-        motion_event.upload_url = GoogleDriveCleanupAction.cleanup(config, motion_event)
+        GoogleDriveCleanupAction.cleanup(config)
 
     @staticmethod
     def do_action(config, motion_event):
         logger.info("Motionevent_id:" + motion_event.event_id + " GoogleDriveCleanupAction event")
-        motion_event.upload_url = GoogleDriveCleanupAction.cleanup(config)
+        GoogleDriveCleanupAction.cleanup(config)
 
     @staticmethod
     def cleanup(config):
-        logger.info("GoogleDriveCleanupAction event")
-
-    @staticmethod
-    def get_list_of_directories(config):
         gauth = GoogleDriveActionBase.GoogleDriveActionBase.authenticate(config)
         drive = GoogleDrive(gauth)
+        retain_from_date = datetime.today() - timedelta(days=int(config.config_obj.get('GoogleDriveUploadAction', 'file_retention_days')))
+
+        file_list = drive.ListFile({'q': "'" + config.config_obj.get('GoogleDriveUploadAction', 'folder') + "' in parents and modifiedDate<'" + retain_from_date.strftime("%Y-%m-%d") + "'", 'maxResults': 1000}).GetList()
+        print(file_list.__sizeof__())
+        for file1 in file_list:
+            logger.debug('GoogleDriveUploadAction Removing: title: %s, id: %s, createdDate: %s, parents: %s' % (file1['title'], file1['id'], file1['createdDate'], file1['parents']))
+            file1.Delete()
+
+
+
+
 
